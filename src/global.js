d61ffd321df6c9d0c8dc47ac5dba35649c3d7624
Matt Born <mattborn@users.noreply.github.com>
2025-10-23 13:42:58 -0500

    Videos!

---
+++ src/global.js
@@ -0,0 +1,21 @@
+//clarity.microsoft.com
+;(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      ;(c[a].q = c[a].q || []).push(arguments)
+    }
+  t = l.createElement(r)
+  t.async = 1
+  t.src = 'https://www.clarity.ms/tag/' + i
+  y = l.getElementsByTagName(r)[0]
+  y.parentNode.insertBefore(t, y)
+})(window, document, 'clarity', 'script', 'tygmid91u1')
+
+// ScrollReveal animations
+const scrollConfig = {
+  cleanup: true,
+  distance: '20%',
+  interval: 50,
+  origin: 'bottom',
+}
